ef2e9e3254a1cd54d6f8bb3b44d1ecd63fbb6f59
DrEhabTurki <89798994+DrEhabTurki@users.noreply.github.com>
2026-05-09 20:22:07 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,23 @@
-// sw.js - ملف الخدمة لجعل التطبيق يعمل أوفلاين وقابل للتثبيت
-const CACHE_NAME = 'sa3aty-v1';
+const CACHE_NAME = 'sa3aty-v3';
 const assets = [
   './',
   './index.html',
   './manifest.json',
-  'https://fonts.googleapis.com/css2?family=Cairo:wght@400;700&display=swap'
+  './logo.png'
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
+self.addEventListener('install', e => {
+  e.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(assets);
     })
   );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
     })
   );
 });
